eba2f5fc3fb80acf3fba25329f4521ef3b0661af
Roman Novikov <mystdeim@gmail.com>
2021-09-17 17:29:57 +0300

    RPN-35: rename

--- nft/api/src/main/kotlin/com/rarible/protocol/nft/api/service/mint/BurnLazyNftValidator.kt
+++ nft/api/src/main/kotlin/com/rarible/protocol/nft/api/service/mint/BurnLazyNftValidator.kt
@@ -1,7 +1,7 @@
 package com.rarible.protocol.nft.api.service.mint
 
 import com.rarible.ethereum.sign.service.ERC1271SignService
-import com.rarible.protocol.dto.LazyNftBodyDto
+import com.rarible.protocol.dto.BurnLazyNftFormDto
 import com.rarible.protocol.nft.api.exceptions.BurnLazyNftIncorrectCreatorsException
 import com.rarible.protocol.nft.api.exceptions.BurnLazyNftIncorrectSignatureException
 import com.rarible.protocol.nft.api.exceptions.LazyItemNotFoundException
@@ -17,20 +17,20 @@ class BurnLazyNftValidator(
     private val lazyNftItemHistoryRepository: LazyNftItemHistoryRepository,
     private val signService: ERC1271SignService
 ) {
-    suspend fun validate(itemId: ItemId, msg: String, lazyNftDto: LazyNftBodyDto) {
+    suspend fun validate(itemId: ItemId, msg: String, burnLazyNftDto: BurnLazyNftFormDto) {
         val lazyMint = lazyNftItemHistoryRepository.findById(itemId).awaitFirstOrNull()
             ?: throw LazyItemNotFoundException(itemId)
 
         val mintCreators = lazyMint.creators.map { it.account }
-        val burnCreators = lazyNftDto.creators
+        val burnCreators = burnLazyNftDto.creators
 
         if (setOf(mintCreators) != setOf(burnCreators)) {
             throw BurnLazyNftIncorrectCreatorsException(itemId)
         }
 
         burnCreators.indices.forEach { i ->
-            val address = lazyNftDto.creators[i]
-            val signature = lazyNftDto.signatures[i]
+            val address = burnLazyNftDto.creators[i]
+            val signature = burnLazyNftDto.signatures[i]
             val hash = Word(Hash.sha3(ERC1271SignService.addStart(msg).bytes()))
             val recovered = signService.recover(hash, signature)
             if (address != recovered) {
